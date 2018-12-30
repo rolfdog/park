7d78d1a06ff0d44f0e104ab809cc6d5d01f765d7
rolfdog <42594053+rolfdog@users.noreply.github.com>
2018-12-29 20:38:32 -0600

    Update patchmulti.sh

--- patchmulti.sh
+++ patchmulti.sh
@@ -11,21 +11,36 @@ echo "Removing any zip file of previous install"
 sudo rm leisure_mn.zip
 wget https://github.com/LeisureCoinProject/LeisureCoin-Core-Masternode/releases/download/v2.0/leisure_mn.zip
 sudo unzip leisure_mn.zip
-sudo mv -f LeisureCoin-cli /usr/local/bin
-sudo mv -f LeisureCoind /usr/local/bin
-sudo mv -f LeisureCoin-tx /usr/local/bin
+sudo mv -f LeisureCoin-cli /home/mn1
+sudo mv -f LeisureCoind /home/mn1
+sudo mv -f LeisureCoin-tx /home/mn1
+sudo mv -f LeisureCoin-cli /home/mn2
+sudo mv -f LeisureCoind /home/mn2
+sudo mv -f LeisureCoin-tx /home/mn2
+sudo mv -f LeisureCoin-cli /home/mn3
+sudo mv -f LeisureCoind /home/mn3
+sudo mv -f LeisureCoin-tx /home/mn3
+sudo mv -f LeisureCoin-cli /home/mn4
+sudo mv -f LeisureCoind /home/mn4
+sudo mv -f LeisureCoin-tx /home/mn4
 sudo rm leisure_mn.zip
 echo -e ""
 echo "! Restarting LeisureCoin Masternode !"
 echo -e ""
-cd ~/.LeisureCoin
+su - mn1 -c './LeisureCoind -daemon'
+su - mn2 -c './LeisureCoind -daemon'
+su - mn3 -c './LeisureCoind -daemon'
+su - mn4 -c './LeisureCoind -daemon'
 sudo rm budget.dat db.log debug.log fee_estimates.dat LeisureCoind.pid mnpayments.dat mncache.dat
 sudo rm -R backups/
 sudo rm -R blocks/
 sudo rm -R chainstate/
 sudo rm -R database/
 sudo rm -R sporks/
-sudo LeisureCoin-cli stop
+su - mn1 -c './LeisureCoin-cli stop'    
+su - mn2 -c './LeisureCoin-cli stop'    
+su - mn3 -c './LeisureCoin-cli stop'    
+su - mn4 -c './LeisureCoin-cli stop'    
 echo ""
 echo "If you see any file not found errors, that is ok!"
 echo ""
