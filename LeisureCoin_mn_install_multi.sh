b8623a862070c5fba7fcdaf4ce647f93fe947f03
rolfdog <42594053+rolfdog@users.noreply.github.com>
2019-01-02 21:07:26 -0600

    Update LeisureCoin_mn_install_multi.sh

--- LeisureCoin_mn_install_multi.sh
+++ LeisureCoin_mn_install_multi.sh
@@ -4,7 +4,9 @@ add-apt-repository ppa:bitcoin/bitcoin && apt-get -y update && apt-get -y instal
 
 echo "Done installing";
 
-dd if=/dev/zero of=/swapfile bs=1M count=4096 && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && /etc/fstab
+dd if=/dev/zero of=/swapfile bs=1M count=4096 && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+
+echo
 
 echo "Setting up user accounts";
 
